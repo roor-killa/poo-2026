8bce239e49928eb457877612b090be0c4c5b94b0
Roor <roland.ratenan@nasdy.fr>
2026-01-21 09:35:40 -0400

    Encapsulation

---
+++ exo_partie3_encapsulation.py
@@ -0,0 +1,21 @@
+class Livre:
+    def __init__(self, titre, auteur, isbn):
+        self._titre = titre
+        self._auteur = auteur
+        self.__isbn = isbn  # Privé
+        self._disponible = True
+    
+    @property
+    def isbn(self):
+        return self.__isbn
+    
+    @property
+    def titre(self):
+        return self._titre
+    
+    @titre.setter
+    def titre(self, nouveau_titre):
+        if len(nouveau_titre) > 0:
+            self._titre = nouveau_titre
+        else:
+            raise ValueError("Le titre ne peut pas être vide")
