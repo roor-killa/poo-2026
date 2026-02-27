759c91ab285d579f4c70c2ff374eefb3c56f5ef5
randi101 <194835331+randi101@users.noreply.github.com>
2026-02-27 13:43:03 -0400

    Modifier le scraper pour prendre tous les liens d'articles de 2 articles

--- TP_POO/tp_scraping/src/scrapers/rci_scraper.py
+++ TP_POO/tp_scraping/src/scrapers/rci_scraper.py
@@ -1,7 +1,9 @@
 
 from base_scraper import BaseScraper
+import time
 
 url_rci = "https://rci.fm/deuxiles/infos/toutes-les-infos"
+url_rci_root = "https://rci.fm"
 
 class RciScraper(BaseScraper):
     def __init__(self, base_url):
@@ -11,20 +13,25 @@ class RciScraper(BaseScraper):
         print(f"Scraping {self.url}")
 
     def parse(self):
-        print("Parsing content")
+        print("Parsing content")      
+
+
 
 
 new_scrap = RciScraper(url_rci)
 soup= new_scrap.fetch_page(new_scrap.base_url)
 
-print()
-print('---------')
 if soup:
     headlines = soup.find_all(attrs={"role": "article"})
-
-    print(headlines)
-    print('------------------------------------')
-    for h in headlines:
-        print(h.attrs['about'])
-
+    for h in headlines[:2]:
+        print('----------------------------------------------------------------------------------------------------------------------------------')
         
+        test_article_url = f"{url_rci_root}{h.attrs['about']}"
+        print(test_article_url)
+        print('Waiting')
+        time.sleep(120)
+
+        new_fetch = new_scrap.fetch_page(test_article_url)
+        new_headlines = new_fetch.find_all(attrs={"role": "article"})
+        for n_h in new_headlines:
+            print(f"{url_rci_root}{n_h.attrs['about']}")
